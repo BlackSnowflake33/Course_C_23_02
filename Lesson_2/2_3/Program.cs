7deb68c3043b5a8358022c2c8b13a41bdc8db5f6
Катя <email@yandex.ru>
2023-02-27 11:52:38 +0300

    1

--- Lesson_2/2_3/Program.cs
+++ Lesson_2/2_3/Program.cs
@@ -3,5 +3,14 @@
 //    34, 5 -> не кратно, остаток 4
 //    16, 4 -> кратно
 
+void Div(int num1, int num2)
+    {
+        if (num1 % num2 ==0)
+            Console.WriteLine("Кратно");
+        else
+            Console.WriteLine("Не кратно " + num1 % num2);
+    }
 
-Console.WriteLine("Hello, World!");
+int a= int.Parse(Console.ReadLine()!);
+int b= int.Parse(Console.ReadLine()!);
+Div(a, b);
