de92ae5a05f7771c9c364a3554b18c4ba23d17e5
Катя <email@yandex.ru>
2023-02-27 20:42:42 +0300

    Промежуточное

--- Lesson_2/Wh/2_2_WH/Program.cs
+++ Lesson_2/Wh/2_2_WH/Program.cs
@@ -1,17 +1,12 @@
 ﻿// Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
 
 
-//int FirstThird(int num)
-//{
-  //  Console.WriteLine(num);
-  //  return num % 10;
-//}
+int FirstThird(int num)
+{
+    Console.WriteLine(num);
+    return ((num/100 % 10) + num %10);
+}
 
 
-//int result = FirstThird(new Random().Next(100, 1000));
-//Console.WriteLine(result);
-int a = 123;
-
-var str = number.ToString();
-
-number = int.Parse(str.Remove(str.Length - 1, 1));
+int result = FirstThird(new Random().Next(100, 1000));
+Console.WriteLine($"Если из техзначного числа удалить вторую цифру, то получится число: {result}");
---
+++ Lesson_2/Wh/2_3_WH/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int FirstThird(int num)
+{
+    Console.WriteLine(num);
+    if(num/1000>0)
+        return num % 10;
+    else 
+     Console.WriteLine("У числа нет третьей цифры");
+}
+
+
+int result = FirstThird(new Random().Next(1, 1000));
+Console.WriteLine($"Третьей цифрой числа является: {result}");
+
+
+
