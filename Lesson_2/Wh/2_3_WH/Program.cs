f7d6d0db359936f1f655ef4d1c5bbea948860553
Катя <email@yandex.ru>
2023-03-01 09:41:40 +0300

    homework

--- Lesson_2/Wh/2_3_WH/Program.cs
+++ Lesson_2/Wh/2_3_WH/Program.cs
@@ -1,17 +1,29 @@
 ﻿// Задача 3: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
-int FirstThird(int num)
+int number =  new Random().Next(1, 10000);
+int count = number.ToString().Length;
+Console.WriteLine($"Случайное число: {number}");
+Console.WriteLine($"Длина числа: {count}");
+
+void Find3Number(int a, int b)
 {
-    Console.WriteLine(num);
-    if(num/1000>0)
-        return num % 10;
-    else 
-     Console.WriteLine("У числа нет третьей цифры");
-}
-
-
-int result = FirstThird(new Random().Next(1, 1000));
-Console.WriteLine($"Третьей цифрой числа является: {result}");
+    if (b<3)
+        Console.WriteLine("Третьей цифры нет");
+    else
+       { 
+        int c = 1;
+        int i=b;
+           while (i > 3)
+            {
+                c = c*10;
+                i--;
+            }
+        int result = (a / (c)) % 10;
+            Console.WriteLine($"Третья цифра числа: {result}");
+       }
+        
+};
+Find3Number(number, count);
 
 
 
