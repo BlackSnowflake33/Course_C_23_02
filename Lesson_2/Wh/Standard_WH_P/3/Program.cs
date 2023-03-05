70553cf1107cc77a1610280160a9b8477eb67561
Катя <email@yandex.ru>
2023-03-05 15:20:25 +0300

    Разбор задач

---
+++ Lesson_2/Wh/Standard_WH_P/3/Program.cs
@@ -0,0 +1,18 @@
+﻿// 3. Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то
+//    программа выводит остаток от деление.
+//    34, 5 -> не кратно, остаток 4
+//    16, 4 -> кратно
+
+void ThirdNum(int num)
+{
+    Console.Write($"{num} -> ");
+    if (num < 100)
+    {
+        Console.WriteLine("The number is not three-digit!");
+        return;
+    }
+    while(num>999) num/=10;
+    Console.WriteLine(num % 10);
+}
+
+ThirdNum(int.Parse(Console.ReadLine()!));
---
+++ Lesson_2/Wh/Standard_WH_P/4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+
+void Weekend(int num)
+{
+    string text = "no";
+
+    if (num == 7 || num == 6) text = "Weekend";
+    else if (num < 6 && num >0) text = "Workday";
+        else text = "not quite a week";
+
+    Console.WriteLine($"{num} -> {text}");
+}
+
+Weekend(int.Parse(Console.ReadLine()!));
