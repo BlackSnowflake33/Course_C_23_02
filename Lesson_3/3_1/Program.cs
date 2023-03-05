fd4aa1a28878018454a5cec1c5da103f7a6b3f9a
Катя <email@yandex.ru>
2023-03-06 01:31:30 +0300

    Lesson 3

---
+++ Lesson_3/3_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая по заданому номеру четверти, показывает диапозон
+//возможных координат точек  в этой четверти
+
+void Quarters(int num)
+{
+    if (num ==1)
+        Console.WriteLine("x>0, y>0");
+    else if (num == 2)
+            Console.WriteLine("x<0, y>0");
+    else if (num == 3)
+            Console.WriteLine("x<0, y<0");
+    else if (num == 4)
+            Console.WriteLine("x>0, y<0");
+    else 
+            Console.WriteLine("Нет такой четверти");
+
+}
+Console.WriteLine("Введите номер четверти: ");
+Quarters(int.Parse(Console.ReadLine()!));
+
---
+++ Lesson_3/3_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход координаты 2-х точек и 
+// находит расстояние между ними в 2D пространстве.
+
+
+Console.WriteLine("Hello, World!");
+
+double Dis(double x1, double y1, double x2, double y2)
+{
+return Math.Sqrt(Math.Pow(x2-x1,2)+ Math.Pow(y2-y1,2));
+}
+Console.WriteLine(Dis(3, 6, 2, 1));
