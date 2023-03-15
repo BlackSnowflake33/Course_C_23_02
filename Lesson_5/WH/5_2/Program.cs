1d82fd9b319f9348b3afd5ffdf5861dbf09d3632
Катя <email@yandex.ru>
2023-03-15 21:28:03 +0300

    Homework 5

---
+++ Lesson_5/WH/5_2/Program.cs
@@ -0,0 +1,44 @@
+﻿//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+int[] InputRandomMassive(int lengthMas, int minRangeMas, int maxRangeMas)
+{
+    int[] masRandom = new int[lengthMas]; // Задаем массив
+    for (int i = 0; i < lengthMas; i++)
+    {
+        masRandom[i] = new Random().Next(minRangeMas, maxRangeMas); // случайные числа в диапозоне     
+    }
+    return masRandom;
+}
+// Вывод массива
+void PrintMassive(int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+        Console.Write($" {mas[i]} ");
+    Console.WriteLine();
+}
+
+int OddNumber(int[] arry)
+{
+    int sumOddnum = 0;
+    Console.WriteLine($"Из массива находящиеся на нечетных позициях: ");
+    for (int i = 0; i < arry.Length; i += 2)
+    {
+        Console.Write($"{arry[i]} ");
+        sumOddnum +=arry[i];
+    }
+    return sumOddnum;
+}
+
+Console.WriteLine("Введите длину массива: ");
+int lengthMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальное значение для диапозона случайного числа: ");
+int minRangeMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное значение для диапозона случайного числа: ");
+int maxRangeMas = int.Parse(Console.ReadLine()!);
+int[] masRandom = InputRandomMassive(lengthMas, minRangeMas, maxRangeMas);
+PrintMassive(masRandom);
+
+
+int result = OddNumber(masRandom);
+Console.WriteLine($"\n{result}");
---
+++ Lesson_5/WH/5_3/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] InputRandomMassive(int lengthMas, int minRangeMas, int maxRangeMas)
+{
+    double[] masRandom = new double[lengthMas]; // Задаем массив
+    Random n_new = new Random();
+    for (int i = 0; i < lengthMas; i++)
+    {
+        masRandom[i] = Math.Round(n_new.NextDouble() * (maxRangeMas + minRangeMas) - maxRangeMas, 2); // случайные числа в диапозоне     
+    }
+    return masRandom;
+}
+// Вывод массива
+void PrintMassive(double[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+        Console.Write($" {mas[i]} ");
+    Console.WriteLine();
+}
+
+double DifferenceMaxMin(double[] arry)
+{
+    double difference = 0;
+    double maxNum = arry[0];
+    double minNum = arry[0];
+
+    for (int i = 0; i < arry.Length; i++)
+    {
+        if (maxNum < arry[i])
+            maxNum = arry[i];
+        else
+            if (minNum > arry[i])
+                minNum = arry[i];
+    }
+    difference = Math.Round(maxNum - minNum, 2);
+    Console.WriteLine($"Mаксимальное элемент массива: {maxNum}. \nМинимальное элемент массива: {minNum}.");
+   
+    return difference;
+}
+
+Console.WriteLine("Введите длину массива: ");
+int lengthMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальное значение для диапозона случайного числа: ");
+int minRangeMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное значение для диапозона случайного числа: ");
+int maxRangeMas = int.Parse(Console.ReadLine()!);
+double[] masRandom = InputRandomMassive(lengthMas, minRangeMas, maxRangeMas);
+PrintMassive(masRandom);
+
+double result = DifferenceMaxMin(masRandom);
+Console.WriteLine($"Разница между максимальным и минимальным элементов массива: {result} ");
