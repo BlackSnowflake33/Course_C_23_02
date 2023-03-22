f122c437db125245ea7759b6970fc724ef8ec003
Катя <email@yandex.ru>
2023-03-23 01:07:24 +0300

    Домашняя к 7 семинару

---
+++ Lesson_7/WH/7_1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+double[,] InputDuoRandomMassive(int rows, int columns, int form, int to)
+{
+    double[,] masDuoRandom = new double[rows, columns];
+    Random n_new = new Random();
+    
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+        {
+            masDuoRandom[i, j] = Math.Round(n_new.NextDouble() * (form + to) - form, 2); 
+        }
+    return masDuoRandom;
+}
+// Вывод массива
+void PrintDuoMassive(double[,] masDuo)
+{
+    for (int i = 0; i < masDuo.GetLength(0); i++)
+    {
+        for (int j = 0; j < masDuo.GetLength(1); j++)
+            Console.Write($" {masDuo[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+
+Console.WriteLine("Введите число строк в двухмерном массиве: ");
+int lineMass = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число столбцов в двухмерном массиве: ");
+int columnMass = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите минимальное значение для диапозона случайного числа: ");
+int minRangeMas = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите максимальное значение для диапозона случайного числа: ");
+int maxRangeMas = int.Parse(Console.ReadLine()!);
+
+double[,] masDuoRandom = InputDuoRandomMassive(lineMass, columnMass, minRangeMas, maxRangeMas);
+PrintDuoMassive(masDuoRandom);
+Console.WriteLine();
+
+
